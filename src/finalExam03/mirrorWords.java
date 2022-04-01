55866edeb51da359e7d53b7d0100e0ba4450dc90
Martin Parunev <parunev@gmail.com>
2022-04-01 14:47:12 +0300

    Updated Mirror Words Problem

--- src/finalExam03/mirrorWords.java
+++ src/finalExam03/mirrorWords.java
@@ -1,4 +1,42 @@
 package finalExam03;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class mirrorWords {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile("(?<symbols>[@#])(?<word>[A-Za-z]{3,})\\1\\1(?<mirror>[A-Za-z]{3,})\\1");
+        Matcher matcher = pattern.matcher(input);
+
+        List<String> matches = new ArrayList<>();
+        int count = 0;
+        while (matcher.find()){
+            String firstWord = matcher.group("word");
+            String secondWord = matcher.group("mirror");
+            count++;
+
+            String reversed = new StringBuilder(secondWord).reverse().toString();
+            if (firstWord.equals(reversed)){
+                matches.add(firstWord + " <=> " + secondWord);
+            }
+        }
+        if (count == 0){
+            System.out.println("No word pairs found!");
+        }else{
+            System.out.printf("%d word pairs found!%n", count);
+        }
+        if (matches.isEmpty()){
+            System.out.println("No mirror words!");
+        }else{
+            System.out.println("The mirror words are:");
+            System.out.println(String.join(", ", matches));
+        }
+    }
 }
